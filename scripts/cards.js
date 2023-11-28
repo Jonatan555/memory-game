6f969acab0eaa1882de70aad7e40086fa2036df2
Jonatan melo <jonatanmelo058@gmail.com>
2023-11-27 21:18:08 -0300

    add click and check cards

--- scripts/cards.js
+++ scripts/cards.js
@@ -8,6 +8,39 @@ function backPage() {
 
 
 function creatCards() {
+    const cardNames = [
+        "card_1",
+        "card_2",
+        "card_3",
+        "card_4",
+        "card_5",
+        "card_6",
+        "card_7",
+        "card_8",
+        "card_9",
+        "card_10",
+        "card_11",
+        "card_12",
+        "card_13",
+        "card_14",
+        "card_15",
+        "card_16",
+        "card_17",
+        "card_18",
+        "card_19",
+    ]
+    
+    const arrayCardsName = cardNames
+    .sort(() => Math.random() - 0.5)
+    .filter((value, index) => index < 12 );
+    
+    
+    const sortedCards =[...arrayCardsName,...arrayCardsName] .sort(
+     () => Math.random() - 0.5
+    );
+    
+
+
     gridCards.innerHTML = "";
     sortedCards.forEach((card) => {
         gridCards.innerHTML += `
@@ -23,6 +56,46 @@ function creatCards() {
     });
 }
 
+function checkMatchCards() {
+    if (firstCard.getAttribute("name") === seconCard.getAttribute("name")) {
+    new Audio("../audios/sci-fi.wav").play();
+    setTimeout(() => {
+        firstCard.classList.add("disabledCard");
+        seconCard.classList.add("disabledCard");
+        firstCard = "";
+        seconCard = "";
+    }, 500);
+    } else {
+      setTimeout(() => {
+        firstCard.classList.remove("flipCard");
+        seconCard.classList.remove("flipCard");
+        firstCard = "";
+        seconCard = "";
+      }, 500);
+
+    }
+}
+
+function clickFlipCard() {
+  const arrayCards = document.querySelectorAll(".card")
+  arrayCards.forEach((card) => {
+    card.addEventListener("click", () => {
+        if (card.classList.contains("flipCard")) return;
+
+        new Audio ("../audios/flip.wav").play();
+
+        if (firstCard === "") {
+            card.classList.add("flipCard");
+            firstCard = card;
+        } else if (seconCard === "") {
+            card.classList.add("flipCard");
+            seconCard = card;
+
+            checkMatchCards();
+        }
+    });
+  });
+}
 
 const playerName = document.querySelector(".playerName");
 const backButton = document.querySelector(".backButton");
@@ -36,35 +109,12 @@ playerName.innerHTML = storagePlayerName;
 backButton.addEventListener("click", backPage);
 
 
-const cardNames = [
-    "card_1",
-    "card_2",
-    "card_3",
-    "card_4",
-    "card_5",
-    "card_6",
-    "card_7",
-    "card_8",
-    "card_9",
-    "card_10",
-    "card_11",
-    "card_12",
-    "card_13",
-    "card_14",
-    "card_15",
-    "card_16",
-    "card_17",
-    "card_18",
-    "card_19",
-]
-
-const arrayCardsName = cardNames
-.sort(() => Math.random() - 0.5)
-.filter((value, index) => index < 12 );
-
-
-const sortedCards =[...arrayCardsName,...arrayCardsName] .sort(
- () => Math.random() - 0.5
-);
 
 creatCards();
+
+let firstCard = "";
+let seconCard = "";
+clickFlipCard();
+
+
+
